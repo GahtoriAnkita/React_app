5b28f0c6eb87f9b23bd97129f41f89d87333b902
GahtoriAnkita <gahtoriankita03@gmail.com>
2024-04-11 21:22:22 +0530

    adding tabs

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import "./App.css"
 import Header from './components/Header'
 import Concept from './components/Concept'
 import { CORE_CONCEPTS } from './components/Data'
+import TabButton from './components/TabButton'
 function App() {
   return (
     <div className='App'>
       <Header />
-      <section>
+      <section style={{ position: "relative" }}>
         <div className='core'>
           <h4 style={{ fontSize: "20px", fontWeight: "700" }}>Core Concepts</h4>
           <div className='comp'>
@@ -16,6 +17,15 @@ function App() {
             <Concept {...CORE_CONCEPTS[2]} />
             <Concept {...CORE_CONCEPTS[3]} />
           </div>
+        </div>
+      </section>
+      <section id='examples'>
+        <div className='exam-block'>
+          <h3>Examples</h3>
+          <TabButton >Components</TabButton>
+          <TabButton>JSX</TabButton>
+          <TabButton>Props</TabButton>
+          <TabButton>State</TabButton>
         </div>
       </section>
     </div>
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TabButton({children}) {
+  return (
+    <>
+    <button id='exam-btn' >{children}</button>
+    </>
+  )
+}
+
+export default TabButton
