f41c6d786e3b18b393b3cae24c394e0628ec6ec2
GahtoriAnkita <gahtoriankita03@gmail.com>
2024-04-16 07:53:52 +0530

    updating state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./App.css"
 import Header from './components/Header'
 import Concept from './components/Concept'
 import { CORE_CONCEPTS } from './components/Data'
 import TabButton from './components/TabButton'
-function handleClick(selectedButton) {
-  //selected contains string --- "Components","JSX","Props","States" -----
-  console.log(selectedButton);
-}
+
+
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("Please click button");
+
+  // let tabContent = "Please click button";
+
+  function handleClick(selectedButton) {
+    //selected contains string --- "Components","JSX","Props","States" -----
+    // tabContent=selectedButton;
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
+  console.log("App Component executing");
   return (
     <div className='App'>
       <Header />
@@ -23,14 +32,15 @@ function App() {
           </div>
         </div>
       </section>
-      <section id='examples'>
+      <section id='examples' style={{ color: "white" }}>
         <div className='exam-block'>
           <h3>Examples</h3>
-          <TabButton onSelect={()=>handleClick("components")} >Components</TabButton>
-          <TabButton onSelect={()=>handleClick("jsx")}>JSX</TabButton>
-          <TabButton onSelect={()=>handleClick("props")}>Props</TabButton>
-          <TabButton onSelect={()=>handleClick("states")}>State</TabButton>
+          <TabButton onSelect={() => handleClick("components")} >Components</TabButton>
+          <TabButton onSelect={() => handleClick("jsx")}>JSX</TabButton>
+          <TabButton onSelect={() => handleClick("props")}>Props</TabButton>
+          <TabButton onSelect={() => handleClick("states")}>State</TabButton>
         </div>
+        {selectedTopic}
       </section>
     </div>
   )
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 function TabButton({children, onSelect}) {
+  console.log("TabButton App Component executing");
   return (
     <>
     <button id='exam-btn' onClick={onSelect} >{children}</button>
